60bd97d18a95cc21df500af9cc446b71ff39126f
Sparsh <sparshgoelk@gmail.com>
2024-11-12 20:16:25 +0530

    added food item adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
-
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Order from './pages/Orders/Order';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <div className="app">
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Order />} />
+        </Routes>
       </div>
     </div>
   );
